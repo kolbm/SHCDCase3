3e2492bd7bbeaebfa3c4e016a1cae62baa115080
Marc Kolb <kolbm@dunellenschools.org>
2025-02-11 08:20:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,40 @@
-st.markdown(f"""
-    <div id="player"></div>
-    <script>
-      var tag = document.createElement('script');
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+if 'video_id' in locals() and 'start_time' in locals() and 'end_time' in locals():
+    st.markdown(f"""
+        <div id="player"></div>
+        <script>
+          var tag = document.createElement('script');
+          tag.src = "https://www.youtube.com/iframe_api";
+          var firstScriptTag = document.getElementsByTagName('script')[0];
+          firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-      var player;
-      function onYouTubeIframeAPIReady() {{
-        player = new YT.Player('player', {{
-          height: '315',
-          width: '560',
-          videoId: '{video_id}',
-          playerVars: {{ 'start': {start_time}, 'autoplay': 1, 'controls': 0 }},
-          events: {{
-            'onStateChange': onPlayerStateChange
+          var player;
+          function onYouTubeIframeAPIReady() {{
+            player = new YT.Player('player', {{
+              height: '315',
+              width: '560',
+              videoId: '{video_id}',
+              playerVars: {{ 'start': {start_time}, 'autoplay': 1, 'controls': 0 }},
+              events: {{
+                'onStateChange': onPlayerStateChange
+              }}
+            }});
           }}
-        }});
-      }}
 
-      function onPlayerStateChange(event) {{
-        if (event.data == YT.PlayerState.PLAYING) {{
-          var end_time = {end_time};
-          var interval = setInterval(function() {{
-            if (player.getCurrentTime() >= end_time) {{
-              player.pauseVideo();
-              clearInterval(interval);
+          function onPlayerStateChange(event) {{
+            if (event.data == YT.PlayerState.PLAYING) {{
+              var end_time = {end_time};
+              var interval = setInterval(function() {{
+                if (player.getCurrentTime() >= end_time) {{
+                  player.pauseVideo();
+                  clearInterval(interval);
+                }}
+              }}, 1000);
             }}
-          }}, 1000);
-        }}
-      }}
-    </script>
-""", unsafe_allow_html=True)
+          }}
+        </script>
+    """, unsafe_allow_html=True)
 
-if not result.empty:
+if 'result' in locals() and not result.empty:
     st.subheader("Matching Location")
     st.write(f"<p class='narrative-text'>{result.iloc[0]['Location']}</p>", unsafe_allow_html=True)
 
